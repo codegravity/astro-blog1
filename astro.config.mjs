d425adef0830f5e87dc0017bc100de4018566dc1
Brett Simpson <brett@codegravity.se>
2025-10-22 19:54:20 +0200

    added breadcrumbs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
