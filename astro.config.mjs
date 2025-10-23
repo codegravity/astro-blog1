ca6d712f515ba8aef4b98acf3b622c112592015f
Brett Simpson <brett@codegravity.se>
2025-10-23 10:09:50 +0200

    AWS adapeter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import awsAmplify from 'astro-aws-amplify';
 import node from '@astrojs/node';
 
 // https://astro.build/config
@@ -12,7 +13,11 @@ export default defineConfig({
   },
   site: 'https://localhost:4321',
   integrations: [sitemap(), robotsTxt()],
-
+  
+  output: "server",
+  adapter: awsAmplify(),
  
-
+/*   adapter: node({
+    mode: 'standalone',
+  }), */
 });
